204a8f17cf94a092ddff4ad5ad66a5ec45f9cbb4
root <root@becarios-3.novalocal>
2023-03-24 16:33:37 +0000

    Version 7

--- Models/VuelosModel.php
+++ Models/VuelosModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class NewsModel extends Model
+class VuelosModel extends Model
 {
     protected $table = 'Vuelos';
     protected $primaryKey = 'IDVuelo';
--- Views/templates/reservar.php
+++ Views/templates/reservar.php
@@ -30,83 +30,19 @@
         </tr>
         </thead>
         <tbody>
+  <?php foreach ($vuelos as $vuelo) : ?>
         <tr>
-            <form name="VA43" method="post" action="compra.php">
-                <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
-                <td>43</td>
-                <td>Virgin America</td>
-                <td>1:43 AM</td>
-                <td>9:45 PM</td>
-                <td>$472.56</td>
-                <input type="hidden" value="43" name="flight">
-                <input type="hidden" value="472.56" name="price">
-                <input type="hidden" value="Virgin America" name="airline">
-                <input type="hidden" name="fromPort" value="Paris"/>
-                <input type="hidden" name="toPort" value="Buenos Aires"/>
-            </form>
+	<form name="VA43" method="post" action="compra.php">
+           <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
+            <td><?= $vuelo['IDVuelo'] ?></td>
+            <td><?= $vuelo['Aerolinea'] ?></td>
+            <td><?= $vuelo['HoraDespeje'] ?></td>
+            <td><?= $vuelo['HoraAterrizaje'] ?></td>
+            <td><?= $vuelo['Precio'] ?></td>
         </tr>
-        <tr>
-            <form name="UA234" method="post" action="compra.php">
-                <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
-                <td>234</td>
-                <td>United Airlines</td>
-                <td>7:43 AM</td>
-                <td>10:45 PM</td>
-                <td>$432.98</td>
-                <input type="hidden" value="234" name="flight">
-                <input type="hidden" value="432.98" name="price">
-                <input type="hidden" value="United Airlines" name="airline">
-                <input type="hidden" name="fromPort" value="Paris"/>
-                <input type="hidden" name="toPort" value="Buenos Aires"/>
-            </form>
-        </tr>
-        <tr>
-            <form name="AL969" method="post" action="compra.php">
-                <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
-                <td>9696</td>
-                <td>Aer Lingus</td>
-                <td>5:27 AM</td>
-                <td>8:22 PM</td>
-                <td>$200.98</td>
-                <input type="hidden" value="9696" name="flight">
-                <input type="hidden" value="200.98" name="price">
-                <input type="hidden" value="Aer Lingus" name="airline">
-                <input type="hidden" name="fromPort" value="Paris"/>
-                <input type="hidden" name="toPort" value="Buenos Aires"/>
-            </form>
-
-        </tr>
-        <tr>
-            <form name="VA12" method="post" action="compra.php">
-                <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
-                <td>12</td>
-                <td>Virgin America</td>
-                <td>11:23 AM</td>
-                <td>1:45 PM</td>
-                <td>$765.32</td>
-                <input type="hidden" value="12" name="flight">
-                <input type="hidden" value="765.32" name="price">
-                <input type="hidden" value="Virgin America" name="airline">
-                <input type="hidden" name="fromPort" value="Paris"/>
-                <input type="hidden" name="toPort" value="Buenos Aires"/>
-            </form>
-        </tr>
-        <tr>
-            <form name="L4346" method="post" action="compra.php">
-                <td><input type="submit" class="btn btn-small" value="Choose This Flight"></td>
-                <td>4346</td>
-                <td>Lufthansa</td>
-                <td>1:45 AM</td>
-                <td>8:34 PM</td>
-                <td>$233.98</td>
-                <input type="hidden" value="4346" name="flight">
-                <input type="hidden" value="233.98" name="price">
-                <input type="hidden" value="Lufthansa" name="airline">
-                <input type="hidden" name="fromPort" value="Paris"/>
-                <input type="hidden" name="toPort" value="Buenos Aires"/>
-            </form>
-        </tr>
-        </tbody>
+	</form>
+        <?php endforeach; ?>
+    </tbody>
     </table>
     <input type="hidden" name="fromPort" value="Paris"/>
     <input type="hidden" name="toPort" value="Buenos Aires"/>
