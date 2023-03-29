225de58877eb5a79a77a31e16efec8b84c39e82b
root <root@becarios-3.novalocal>
2023-03-29 18:46:38 +0000

    Version 11

--- Models/VuelosModel.php
+++ Models/VuelosModel.php
@@ -8,5 +8,16 @@ class VuelosModel extends Model
 {
     protected $table = 'Vuelos';
     protected $primaryKey = 'IDVuelo';
-    protected $allowedFields = ['Aerolinea', 'HoraDespeje', 'HoraAterrizaje', 'Precio'];
+    protected $allowedFields = ['Aerolinea', 'HoraDespeje', 'HoraAterrizaje','PaisOrigen','PaisDesino', 'Precio'];
+
+public function getVuelos($depature, $dest)
+{
+    $builder = $this->db->table('Vuelos');
+    $builder->where('PaisOrigen', $depature);
+    $builder->where('PaisDestino', $dest);
+    $query = $builder->get();
+    return $query->getResultArray();
 }
+}
+
+
